14101aed5f081da417229ef622dfc482c2e0a09a
Sanjaykanur <sanjaykumar8667349532@gmail.com>
2026-02-06 15:27:52 +0530

    Third Commit

--- src/main/java/com/gitup/A.java
+++ src/main/java/com/gitup/A.java
@@ -6,10 +6,29 @@ public class A {
 
     int c = a+b;
 
+    int  d = a+b;
+    int e = a+b;
+    int f = a+b;
+
     public int getC() {
         return c;
-        int d = 14;
-        int e = c-d;
+
+    }
+
+    public int getD() {
         return d;
+    }
+
+    public int getE() {
+        return e;
+    }
+    public int getF() {
+        return f;
+    }
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
     }
 }
